8a6437ffecd1f3e8e070993b178546f53a433b0b
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-15 00:41:52 +0530

    learned findOne

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,11 @@ router.get("/", function(req,res){
   res.render("index");
 })
 
+router.get("/allusers", async function(req,res){
+  let allusers = await userModel.findOne({username:"harsh"});
+  res.send(allusers);
+})
+
+
+
 module.exports = router;
