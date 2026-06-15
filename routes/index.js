369946d5c02d5d82036a37e47fd5a29060559c25
Ravi Hiremath <hiremathravi333@gmail.com>
2026-06-16 00:28:54 +0530

    cookie setup reading and deleting

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,22 @@ var router = express.Router();
 
 
 router.get("/", function(req,res){
-  req.session.ban = true ;
+  res.cookie("age",25);
   res.render("index");
 })
 
-
-router.get("/checkban", function(req,res){
-  if(req.session.ban === true){
-     res.send("you are banned");
-  }
-  else{
-    res.send("not banned");
-  }
+router.get("/read", function(req,res){
+ console.log(req.cookies.age);
+ res.send("check");
 });
 
-router.get("/removeban", function(req,res){
- req.session.destroy(function(err){
-if (err) throw err ;
-res.send("ban  removed");
- });
-  });
+router.get("/delete", function(req,res){
+ res.clearCookie("age");
+ res.send("clear hogayi");
+});
+
+
+
 
 
 
